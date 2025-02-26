57d6c1cc0d7f83af82e75c202796b20f823eb590
Sonny Nguyen <sonnynguyen.150801@gmail.com>
2025-02-26 16:11:20 +0700

    Update Installation setup

--- Installation/Maya/maya_setup.bat
+++ Installation/Maya/maya_setup.bat
@@ -8,15 +8,19 @@ set "BAT_DIR=%~dp0"
 for %%I in ("%BAT_DIR%\..\..") do set "GSTOOLS_ROOT=%%~fI"
 
 :: Construct the path to user setup 2018
-set "source_path_users2018=%GSTOOLS_ROOT%\Softwares\Maya\2018\scripts"
+set "source_path_users2018=%GSTOOLS_ROOT%\Softwares\Maya\2018"
+
+:: Construct the path to application plugins
+set "source_path_applicationplugins=%GSTOOLS_ROOT%\Softwares\Maya\ApplicationPlugins"
 
 :: Construct the path to modules
 set "source_path_modules=%GSTOOLS_ROOT%\Softwares\Maya\modules"
 
 :: Define source and destination paths
-set "destination_path_users2018=%USERPROFILE%\Documents\maya\2018\scripts"
+set "destination_path_users2018=%USERPROFILE%\Documents\maya\2018"
 
-:: Define source and destination paths
+set "destination_path_applicationplugins=C:\ProgramData\Autodesk\ApplicationPlugins"
+
 set "destination_path_modules=%USERPROFILE%\Documents\maya\modules"
 
 :: Check if source directory exists
@@ -30,6 +34,9 @@ if exist "%destination_path_modules%\*" del /s /q "%destination_path_modules%\*"
 for /d %%i in ("%destination_path_modules%\*") do rmdir /s /q "%%i"
 
 :: Copy all files and subdirectories
-xcopy "%source_path_modules%\*" "%destination_path_modules%"
+xcopy "%source_path_users2018%\*" "%destination_path_users2018%" /Y /E /I
 
-xcopy "%source_path_users2018%\*" "%destination_path_users2018%"
+xcopy "%source_path_applicationplugins%\*" "%destination_path_applicationplugins%" /Y /E /I
+
+xcopy "%source_path_modules%\*" "%destination_path_modules%" /Y /E /I
+
--- Softwares/Maya/2018/scripts/userSetup.py
+++ Softwares/Maya/2018/scripts/userSetup.py
@@ -1,8 +1,9 @@
 import maya.utils
 import sys
 
+sys.path.append("//192.168.1.10/Softwares/Technical_Script/Maya_2018/MayaTools/scripts/")
+
 def addPaths():
-    sys.path.append("//192.168.1.10/Softwares/Technical_Script/Maya_2018/MayaTools/scripts/")
     sys.path.append("//192.168.1.10/Softwares/Pipeline/GSTools/Softwares/Maya/scripts/GSTools")
     
     # Import your menu creation script
