554274928c6c9b6628b6e1b59523601e58e4838a
Sonny Nguyen <sonnynguyen.150801@gmail.com>
2026-06-12 10:11:19 +0700

    Shelf fix + install_server relocaton

--- Deploy/install_server.bat
+++ Deploy/install_server.bat
@@ -1,5 +1,5 @@
 @echo off
 :: Maya user install - thin wrapper. All logic lives in _core\install_core.bat.
 :: Source: <root>\Environment\Autodesk\Maya   Dest: %USERPROFILE%\Documents\maya
-call "%~dp0\_core\install_core.bat" "\" "\\192.168.1.210\Pipeline\gs-tool-dcc"
+call "%~dp0\_core\install_core.bat" "\" "\\192.168.1.210\Pipeline\Tool\gs-tool-dcc"
 exit /b %ERRORLEVEL%
